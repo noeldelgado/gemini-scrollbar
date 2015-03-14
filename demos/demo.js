e7113041dea72c7f93272423c6cdf0b194bcbbf6
Noel Delgado <auhcsei@gmail.com>
2015-03-13 21:54:46 -0600

    add / demos

---
+++ demos/demo.js
@@ -0,0 +1,31 @@
+/* var GeminiScrollbar = require('../'); */
+
+var ex1 = new GeminiScrollbar({
+    element: document.querySelector('#example-1')
+})
+
+var ex2 = new GeminiScrollbar({
+    element: document.querySelector('#example-2'),
+    autoshow: true
+})
+
+var ex3 = new GeminiScrollbar({
+    element: document.querySelector('#example-3')
+})
+document.querySelector('#example-3 img').addEventListener('load', function() {
+    ex3.update()
+})
+
+var ex4 = new GeminiScrollbar({
+    element: document.querySelector('#example-4'),
+    autoshow: true
+})
+
+var ex5 = new GeminiScrollbar({
+    element: document.querySelector('#example-5'),
+    horizontal : true
+})
+
+var ex6 = new GeminiScrollbar({
+    element: document.querySelector('#example-6'),
+})
