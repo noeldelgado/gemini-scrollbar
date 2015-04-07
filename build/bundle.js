8061675697a2a29f67ae1baa5f3239949d7f4656
Noel Delgado <auhcsei@gmail.com>
2015-04-06 22:44:06 -0500

    update description

--- build/bundle.js
+++ build/bundle.js
@@ -1,7 +1,7 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /**
  * gemini-scrollbar
- * @version 1.0.2
+ * @version 1.1.2
  * @link http://noeldelgado.github.io/gemini-scrollbar/
  * @license MIT
  */
@@ -217,8 +217,11 @@
         x = ((viewElement.scrollLeft * 100) / viewElement.clientWidth);
 
         this._thumbVerticalElement.style.msTransform = 'translateY(' + y + '%)';
+        this._thumbVerticalElement.style.webkitTransform = 'translateY(' + y + '%)';
         this._thumbVerticalElement.style.transform = 'translateY(' + y + '%)';
+
         this._thumbHorizontalElement.style.msTransform = 'translateX(' + x + '%)';
+        this._thumbHorizontalElement.style.webkitTransform = 'translateX(' + x + '%)';
         this._thumbHorizontalElement.style.transform = 'translateX(' + x + '%)';
     };
 
