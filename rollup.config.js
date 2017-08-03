aec82b03ed61e995b0c0220d84ee055bc0c7ad1f
Noel Delgado <auhcsei@gmail.com>
2017-08-02 22:15:27 -0500

    lazy progress

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,23 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 import buble from 'rollup-plugin-buble';
 
-export default {
-  entry: './src/index.js',
-  format: 'umd',
-  moduleName: 'GeminiScrollbar',
-  plugins: [buble()],
-  dest: './lib/index.js'
-};
+export default [
+  {
+    entry: 'src/index.js',
+    dest: 'lib/index.umd.js',
+    format: 'umd',
+    moduleName: 'GeminiScrollbar',
+    plugins: [
+      resolve(),
+      commonjs(),
+      buble()
+    ]
+  },
+  {
+    entry: 'src/index.js',
+    dest: 'lib/index.es.js',
+    format: 'es',
+    external: ['dpr-change']
+  }
+];
